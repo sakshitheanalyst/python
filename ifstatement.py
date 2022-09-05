453f57a9533e043cd8d1fd8892be28292dec8b3b
sakshitheanalyst <112562289+sakshitheanalyst@users.noreply.github.com>
2022-09-05 22:01:23 +0530

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,25 @@
+# n=input("enter a number")
+# n=int(n)
+# if n%2==0:
+#     print("number is even")
+# else:
+#     print("number is odd")
+india=["mumbai", "banglore", "chennai", "delhi"]
+pakistan=["karachi", "lahore", "islamabad"]
+bangladesh=["dhaka", "khilna", "rangpur"]
+
+# city = input("enter city name: ")
+#
+# if city in india:
+#     print(f"{city}is in india")
+# elif city in pakistan:
+#     print(f"{city} is in pakistan")
+# elif city in bangladesh:
+#     print(f"{city} is in bangladesh")
+# else:
+#     print(f"I won't be able to tell you which country {city} is in! sorry")
+# city1 = input("enter city 1:")
+# city2 = input("enter city 2:")
+# if city1 in india and city2 in india:
+#     print("both cities i
+#
